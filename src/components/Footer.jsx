702057e670b7e0987ffe9b09bab6bfbe1c891a74
poorvesh <poorveshgediya@gmail.com>
2025-07-15 19:31:48 +0530

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,48 +1,44 @@
-import React from 'react'
+import React from "react";
 import style from "../style/Footer.module.css";
 import {
-    FaEnvelope,
-    FaFacebook,
-    FaGithub,
-    FaLinkedin,
-    FaWhatsapp,
-    FaXTwitter,
-  } from "react-icons/fa6";
+  FaEnvelope,
+  FaFacebook,
+  FaGithub,
+  FaLinkedin,
+  FaWhatsapp,
+  FaXTwitter,
+  FaPinterest,
+  FaMedium,
+} from "react-icons/fa6";
 
 function Footer() {
   return (
     <div>
-        <footer className={style.soc_media}>
-            <a
-              id={style.git_hub}
-              href="https://github.com/poorveshgediya"
-              target="_blank"
-            >
-              <FaGithub/>
-            </a>
-            <a
-              id={style.linked_in}
-              href="https://www.linkedin.com/in/purveshgediya/"
-              target="_blank"
-            >
-              <FaLinkedin />
-            </a>
-            <a id={style.whats_app} href="https://wa.me/7861851386" target="_blank">
-              <FaWhatsapp />
-            </a>
-            <a
-              id={style.gmail}
-              href="mailto:poorveshgediya@gmail.com"
-              target="_blank"
-            >
-              <FaEnvelope />
-            </a>
-            <a id={style.twitter} href="https://x.com/GediyaPoorvesh" target="_blank">
-              <FaXTwitter />
-            </a>
-          </footer>
+      <footer className={style.soc_media}>
+        <a href="https://github.com/poorveshgediya" target="_blank">
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/purveshgediya/" target="_blank">
+          <FaLinkedin />
+        </a>
+        <a href="https://wa.me/7861851386" target="_blank">
+          <FaWhatsapp />
+        </a>
+        <a href="mailto:poorveshgediya@gmail.com" target="_blank">
+          <FaEnvelope />
+        </a>
+        <a href="https://x.com/GediyaPoorvesh" target="_blank">
+          <FaXTwitter />
+        </a>
+        <a href="https://medium.com/@poorveshgediya" target="_blank">
+          <FaMedium />
+        </a>
+        <a href="https://in.pinterest.com/PurveshGediya" target="_blank">
+          <FaPinterest />
+        </a>
+      </footer>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
