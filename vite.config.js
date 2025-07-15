1254cf9c40e05d51865ceb87c7283653bee7695b
poorvesh <poorveshgediya@gmail.com>
2025-07-15 19:46:43 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/purvesh-sPortfolio.github.io/',
   server: {
     host: '0.0.0.0',
     port: 5173
