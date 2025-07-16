3d15635f96190ab1215fb1026bfbaaea3431e3c6
poorvesh <poorveshgediya@gmail.com>
2025-07-16 12:12:31 +0530

    updated

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,17 +3,19 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import style from "../style/experience.module.css";
 import logo1 from "../photo/cct.png";
-import logo2 from "../photo/cognifyz.png"; 
+import logo2 from "../photo/cognifyz.png";
 import logo3 from "../photo/ascendEX.jpg";
 const experience = [
   {
     logo: logo3,
     company: "AscendEX",
     position: "Affiliate Program | (March 2025 - Present)",
-    description:"Collaborating with AscendEX’s affiliate program to promote the platform among indian audiences. Creating and sharing educational content across social platforms to onboard new users."  },
+    description:
+      "Collaborating with AscendEX’s affiliate program to promote the platform among indian audiences. Creating and sharing educational content across social platforms to onboard new users.",
+  },
   {
     logo: logo1,
-    company: "Calanjiyam Consultancies and Technologies",
+    company: "Calanjiyam Consultancies & Technologies",
     position: "Web developer Intern | (April - June 2025)",
     description:
       "• Contributing in our group project which is “ web based coffee billing system ” \n • I’m handle the “ product management module ”.\n • Created UI for display product with details and also can add manually and using HTML-CSS, JavaScript, PHP, Mysql.",
@@ -22,7 +24,8 @@ const experience = [
     logo: logo2,
     company: "Cognifyz Technologies",
     position: "Web developer Intern | (September - October 2024)",
-    description:"web developer intern during the internship and complete the tasks which is assigned by the mentor."  
+    description:
+      "web developer intern during the internship and complete the tasks which is assigned by the mentor.",
   },
 ];
 
@@ -35,25 +38,29 @@ function Experience() {
         <div className={style.experiencecontent}>
           {experience.map((experience, index) => (
             <React.Fragment key={index}>
-            <div className={style.box}>
-              <img
-                className={style.cctlogo}
-                src={experience.logo}
-                alt="Company Logo"
-              />
-              <h3>{experience.company}</h3>
-              <h4>{experience.position}</h4>
+              <div className={style.box}>
+                <div className={style.topcontent}>
+                  <img
+                    className={style.cctlogo}
+                    src={experience.logo}
+                    alt="Company Logo"
+                  />
+                  <div className={style.companydetails}>
+                    <h3>{experience.company}</h3>
+                    <h4>{experience.position}</h4>
+                  </div>
+                </div>
+                <br />
+                <p>
+                  {experience.description.split("\n").map((line, idx) => (
+                    <span key={idx}>
+                      {line}
+                      <br />
+                    </span>
+                  ))}
+                </p>
+              </div>
               <br />
-              <p>
-                {experience.description.split("\n").map((line, idx) => (
-                  <span key={idx}>
-                    {line}
-                    <br />
-                  </span>
-                ))}
-              </p>
-            </div>
-            <br />
             </React.Fragment>
           ))}
         </div>
